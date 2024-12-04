9117ce4c256866b8a675e3e8cd6fbc8317d47388
Mek-d1 <davidpromise2003@gmail.com>
2024-12-04 07:00:20 +0100

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -12,6 +12,7 @@ const {
 const { search, download } = require("aptoide-scraper");
 const googleTTS = require("google-tts-api");
 const ytdl = require("X-bot");
+const getFBInfo = require("@xaviabot/fb-downloader");
 const yts = require("secktor-pack");
 const fs = require("fs-extra");
 const axios = require("axios");
