038153f83dedfd2e962213a48f63e53551763bac
Mek-d1 <davidpromise2003@gmail.com>
2024-12-05 14:47:00 +0100

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -9,6 +9,7 @@ const moment = require('moment-timezone')
 const {fetchJson,smd, tlang,send, getBuffer, prefix, Config ,groupdb } = require('../lib')
 let gis = require("async-g-i-s");
 const axios = require('axios')
+const Genius = require("genius-lyrics");  
 const fetch = require('node-fetch')
 
    //---------------------------------------------------------------------------
@@ -145,7 +146,35 @@ ${Lyrics}
   }
 )
            
+smd(
+  {
+    pattern: "lyrics",
+    desc: "Get the lyrics of a song.",
+    category: "search",
+    filename: __filename,
+    use: "<song_name>",
+  },
+   async (context) => {
 
+const { client, m, text } = context;
+
+
+const Genius = require("genius-lyrics");  const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO"); 
+
+ try { 
+ if (!text) return m.reply("Provide a song name!"); 
+ const searches = await Client.songs.search(text); 
+ const firstSong = searches[0]; 
+
+ const lyrics = await firstSong.lyrics(); 
+ await client.sendMessage(m.chat, { text: lyrics}, { quoted: m }); 
+ } catch (error) { 
+             m.reply(`I did not find any lyrics for ${text}. Try searching a different song.`); 
+             console.log(error); 
+         } 
+
+   }
+   )
 
     //---------------------------------------------------------------------------
 smd({
