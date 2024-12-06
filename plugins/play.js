bbb936d8819e89506e21e3d6b66e8c6a2795526a
Mek-d1 <davidpromise2003@gmail.com>
2024-12-06 09:12:15 +0100

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -148,8 +148,8 @@ smd({
         const _0x509920 = _0x2cc463.data;
         console.log("API Response:", _0x509920);
 
-        if (_0x509920.status && _0x509920.result.url) {
-          const _0x539170 = _0x509920.audio.url;
+        if (_0x509920.status && _0x509920.data.url) {
+          const _0x539170 = _0x509920.data.url;
           
           // Download the mp3 file
           const _0x3ce5d2 = await axios({
