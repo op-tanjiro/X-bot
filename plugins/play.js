04d73b7a5c209918a5dd12730ed75396a6637bb3
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2024-12-13 11:54:48 +0100

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -226,7 +226,7 @@ smd({
         const _0x509920 = _0x2cc463.data;
         console.log("API Response:", _0x509920);
 
-        if (_0x509920.status && _0x509920.BK9.mediaLink) {
+        if (_0x509920.status !== true && _0x509920.BK9.mediaLink) {
           const _0x539170 = _0x509920.BK9.mediaLink;
           
           // Download the mp4 file
