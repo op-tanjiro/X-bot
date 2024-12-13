7f6d382cbfa67953882ec52d032482ac519aa258
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2024-12-13 12:50:11 +0100

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -382,7 +382,7 @@ smd({
     });
 
     // Use the new API to get download links
-    const downloadApiUrl = "https://www.dark-yasiya-api.site/download/ytmp4?url=" + encodeURIComponent(_0x4f86cb.url);
+    const downloadApiUrl = "https://api.giftedtech.my.id/api/download/ytvideo?apikey=gifted&url=" + encodeURIComponent(_0x4f86cb.url);
     
     let _0x4acf6c = 3; // Retry logic
     while (_0x4acf6c > 0) {
@@ -391,8 +391,8 @@ smd({
         const _0x509920 = _0x2cc463.result;
         console.log("API Response:", _0x509920);
 
-        if (_0x509920.status && _0x509920.result.dl_link) {
-          const _0x539170 = _0x509920.result.dl_link;
+        if (_0x509920.status && _0x509920.result.download_url) {
+          const _0x539170 = _0x509920.result.download_url;
           
           // Download the mp4 file
           const _0x3ce5d2 = await axios({
